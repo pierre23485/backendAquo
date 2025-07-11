c59170ead6e13ab946a3a8e4168ac515b431bbda
Michel <fistonkajibwami2@gmail.com>
2025-07-11 17:01:11 -0330

    add pach

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   optionsSuccessStatus: 200
 };
